dda57c3f904882696e4993f0aab25443c13ba5f2
Pete Thompson <peter.thompson@roswellstudios.com>
2024-08-12 11:52:29 -0400

    feat: whitespace

--- page.php
+++ page.php
@@ -3,14 +3,14 @@
 while (have_posts()) {
     the_post(); ?>
     <div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('images/ocean.jpg') ?>)"></div>
-    <div class="page-banner__content container container--narrow">
-    <h1 class="page-banner__title"><?= the_title(); ?></h1>
-    <div class="page-banner__intro">
-        <p>Don't forget to replace me later.</p>
+        <div class="page-banner__bg-image" style="background-image: url(<?= get_theme_file_uri('images/ocean.jpg') ?>)"></div>
+        <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?= the_title(); ?></h1>
+        <div class="page-banner__intro">
+            <p>Don't forget to replace me later.</p>
+        </div>
+        </div>
     </div>
-    </div>
-</div>
 
 <div class="container container--narrow page-section">
     <?php
